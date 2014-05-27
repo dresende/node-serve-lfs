2e0ae9a0c1f877d86c5eb992db21f243801363a4
Diogo Resende <dresende@thinkdigital.pt>
2014-05-27 19:26:20 +0100

    0.0.1

---
+++ index.js
@@ -0,0 +1,79 @@
+var parseurl = require("parseurl");
+var resolve  = require("path").resolve;
+var send     = require("send");
+var url      = require("url");
+var LFS      = require("lfs");
+
+exports = module.exports = function (paths, options) {
+	options = extend({}, options);
+
+	var redirect = (options.redirect !== false);
+	var lfs      = new LFS({ layer: true, layers: paths });
+
+	options.maxage = (options.maxage || options.maxAge || 0);
+
+	return function staticMiddleware(req, res, next) {
+		if ([ "GET", "HEAD" ].indexOf(req.method) == -1) return next();
+
+		var path        = parseurl(req).pathname;
+		var originalUrl = url.parse(req.originalUrl || req.url);
+
+		if (path == "/" && originalUrl.pathname[originalUrl.pathname.length - 1] != "/") {
+			return directory();
+		}
+
+		function directory() {
+			if (!redirect) return next();
+
+			var target;
+
+			originalUrl.pathname += "/";
+
+			target = url.format(originalUrl);
+
+			res.writeHead(303, "See Other", {
+				"Location": target
+			});
+			return res.end("Redirecting to " + escape(target));
+		}
+
+		function error(err) {
+			if (err.status == 404) return next();
+
+			return next(err);
+		}
+
+		lfs.get(path, function (root) {
+			if (root === null) {
+				res.writeHead(404, "Not Found");
+				return res.end();
+			}
+
+			send(req, path, {
+				maxage : options.maxage,
+				root   : root
+			})
+			.on("error", error)
+			.on("directory", directory)
+			.pipe(res);
+		});
+	};
+};
+
+function escape(html) {
+	return String(html)
+		.replace(/&(?!\w+;)/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;');
+}
+
+function extend(obj, source) {
+	if (!source) return obj;
+
+	for (var prop in source) {
+		obj[prop] = source[prop];
+	}
+
+	return obj;
+}
